ca4c81ab836529570852740b3424c7f2ddace730
Markos Stinis <mark93@hotmail.gr>
2024-06-20 20:42:51 +0300

    updates

--- browser/counter-app/counter.js
+++ browser/counter-app/counter.js
@@ -1,10 +1,10 @@
+const DEFAULT = 0
 let counter = 0
 
 window.addEventListener('DOMContentLoaded', function() {
-    
-    this.document.querySelector('#btnDecr').addEventListener('click', () => onDecreaseClicked)
-    this.document.querySelector('#btnReset').addEventListener('click', () => onResetClicked)
-    this.document.querySelector('#btnIncr').addEventListener('click', () => onIncreaseClicked)
+    this.document.querySelector('#btnDecr').addEventListener('click', () => onDecreaseClicked())
+    this.document.querySelector('#btnReset').addEventListener('click', () => onResetClicked())
+    this.document.querySelector('#btnIncr').addEventListener('click', () => onIncreaseClicked())
 })
 
 /**
@@ -29,4 +29,48 @@ function onResetClicked() {
  */
 function onIncreaseClicked() {
     increaseCounter()
+}
+
+/**
+ * Decreases the counter by one and renders to UI.
+ */
+function decreaseCounter() {
+    counter--
+    showCounter()
+}
+
+/**
+ * Resets the counter to zero and renders to UI.
+ */
+function resetCounter() {
+    counter = DEFAULT
+    showCounter()
+}
+
+/**
+ * Increases the counter by one and renders to UI.
+ */
+function increaseCounter() {
+    counter++
+    showCounter()
+}
+
+/**
+ * Assigns the counter to the corresponding UI element.
+ * And gives styling accordingly.
+ */
+function showCounter() {
+    // Data binding
+    const counterDOM = document.querySelector('#counter')
+    counterDOM.textContent = counter
+
+    // Styling
+    styleCounterElement(counterDOM)
+}
+
+function styleCounterElement(counterDOM) {
+
+    counterDOM.classList.toggle('color-green', counter > 0)
+    counterDOM.classList.toggle('color-red', counter < 0)
+    counterDOM.classList.toggle('color-black', counter === 0)
 }
